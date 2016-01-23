559b2fb6eb29bfe548d0be16c61d42dddd77809d
Mike <ghoti2007@gmail.com>
2016-01-23 18:43:27 -0500

    Catch and run corpstats if a corp is given for who

--- modules/who.py
+++ modules/who.py
@@ -1,11 +1,17 @@
 import arrow
+import evelink.api
+import evelink.corp
 import evelink.eve
+from html.parser import HTMLParser
 import requests
+
+from config.credentials import CORP_KEYID, CORP_VCODE
+
 
 def who(toon):
 
     if not toon:
-        return("NEED A NAME WTF")
+        return 'NEED A NAME WTF'
 
     api = evelink.eve.EVE()
     id = api.character_id_from_name(toon)
@@ -13,14 +19,29 @@ def who(toon):
     if not id.result:
         return "Character not found"
 
-    name, corp, corpsince, alliance, secstatus, age = corpsheet(id, api)
-    stats = killstats(id)
+    # There be dragons here
+    try:
+        # if this breaks, that means someone tried to who a corp, we can handle that
+        name, corp, corpsince, alliance, secstatus, age = corpsheet(id, api)
+        stats = killstats(id)
 
-    if alliance:
-        return "```{} (Born {})\n{} SecStatus with {}\nJoined {} {} ({})```".format(
-                name, age, secstatus, stats, corp, corpsince, alliance)
-    else:
-        return "```{} (Born {})\n{} SecStatus with{}\nJoined {} {}```".format(name, age, secstatus, stats, corp, corpsince)
+        if alliance:
+            return "```{} (Born {})\n{} SecStatus with {}\nJoined {} {} ({})```".format(
+                    name, age, secstatus, stats, corp, corpsince, alliance)
+        else:
+            return "```{} (Born {})\n{} SecStatus with{}\nJoined {} {}```".format(
+                    name, age, secstatus, stats, corp, corpsince)
+    except evelink.api.APIError:
+        try:
+            #and here we fail if we supplied an alliance name, oof
+            name, ticker, alliance, desc, ceo, members = corpinfo(id)
+            return "``` {} <{}>\n{}\nAlliance: {}\nCEO: {}\nMembers: {}```".format(name, ticker, desc, alliance, ceo, members)
+        except evelink.api.APIError:
+            return "Seems you supplied an alliance name, soonTM"
+
+
+
+
 
 def corpsheet(id, api):
     info = api.character_info_from_id(id.result)
@@ -31,6 +52,21 @@ def corpsheet(id, api):
     secstatus = float('%.2f' % info.result['sec_status'])
     age = arrow.get(info.result['history'][-1]['start_ts']).humanize()
     return name, corp, corpsince, alliance, secstatus, age
+
+
+def corpinfo(id):
+    api = evelink.api.API(api_key=(CORP_KEYID, CORP_VCODE))
+    corp = evelink.corp.Corp(api=api)
+    info = corp.corporation_sheet(corp_id=id.result)
+    name = info.result['name']
+    ticker = info.result['ticker']
+    alliance = info.result['alliance']['name']
+    desc = info.result['description']
+    desc = strip_tags(desc)
+    ceo = info.result['ceo']['name']
+    members = info.result['members']['current']
+    return name, ticker, alliance, desc, ceo, members
+
 
 def killstats(id):
     '''
@@ -55,6 +91,28 @@ def killstats(id):
         return ' no reliable killboard data'
 
     return "{} kills and {} losses".format(kills, losses)
+
+
+class MLStripper(HTMLParser):
+    '''
+    Simple, no nonsense, make things unpretty html stripper for descriptions
+    '''
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
 if __name__ == "__main__":
     proper = who('Chainsaw McGinny')
