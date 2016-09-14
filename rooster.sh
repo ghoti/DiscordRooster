1e28ae97b692eff632bd8c1aee92b17558e88137
Mike <ghoti2007@gmail.com>
2016-09-14 04:23:44 -0400

    Remove unused and accidentally commited files cleanup startup script

--- rooster.sh
+++ rooster.sh
@@ -1,4 +1,6 @@
-until python rooster.py do;
-    echo "Rooster crashed with exit code $?... Respawning" >&2
-    sleep 2
+until python rooster.py; do
+    echo "Rooster crashed with exit code" >&2
+    echo $? >&2
+    echo "... Respawning" >&2
+    sleep 2;
 done
