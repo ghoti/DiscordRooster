73c1bcec689b640e8cc2bdbccb09fcc1e22c1fd5
Frosty-nee <frostbite@frosty-nee.net>
2017-02-27 11:15:55 +0000

    added total value to buyback command

--- modules/buyback.py
+++ modules/buyback.py
@@ -6,9 +6,9 @@ import evelink.eve
 from config.credentials import FWEIGHT_KEYID, FWEIGHT_VCODE
 
 def buyback():
-    outstanding = parse_contracts(get_contracts())
+    outstanding, value = parse_contracts(get_contracts())
 
-    return '```Outstanding Buyback Contracts: {}```'.format(outstanding)
+    return '```Outstanding Buyback Contracts: {}\nTotal value: {:,}```'.format(outstanding, value)
 
 def get_contracts():
     api = evelink.api.API(api_key=(FWEIGHT_KEYID, FWEIGHT_VCODE))
@@ -18,11 +18,14 @@ def get_contracts():
 
 def parse_contracts(contracts):
     outstanding = 0
+    value = 0
     for contract in contracts:
-        if contracts[contract]['type'] == 'Item Exchange':
+        if contracts[contract]['type'] == 'ItemExchange':
             if contracts[contract]['status'] == 'Outstanding':
-                outstanding += 1
-    return outstanding
+                if contracts[contract]['issuer'] != 95548667:
+                    outstanding += 1
+                    value += contracts[contract]['price']
+    return outstanding, value
 
 if __name__ == "__main__":
     print(buyback())
