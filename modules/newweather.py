4261dd1f7213d2ff10b17c3a779255e377ef87a8
Mike <ghoti2007@gmail.com>
2017-07-25 21:16:45 -0400

    Add back celsius for weather

--- modules/newweather.py
+++ modules/newweather.py
@@ -28,9 +28,9 @@ def weather(city=None):
         #embed.set_footer(text='Powered by Dark Sky', icon_url='https://darksky.net/images/darkskylogo.png')
         embed.description = fore.daily.summary
         #embed.add_field(name='Daily Summary:', value=fore.daily.summary)
-        embed.add_field(name='Current Conditions:', value='{}\n{}F (feels like {}F) at {}% Humidity'.format(
-            fore.currently.summary, fore.currently.temperature, fore.currently.apparentTemperature,
-            int(fore.currently.humidity*100)))
+        embed.add_field(name='Current Conditions:', value='{}\n{}F/{:.1f}C (feels like {}F/{:.1f}C) at {}% Humidity'.format(
+            fore.currently.summary, fore.currently.temperature, (fore.currently.temperature-32)*(5/9),
+            fore.currently.apparentTemperature, (fore.currently.apparentTemperature-32)*(5/9), int(fore.currently.humidity*100)))
 
         if hasattr(fore, 'alerts'):
             embed.color = Color.red()
